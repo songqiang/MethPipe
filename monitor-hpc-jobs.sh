74c0b06881574256eddb5b8e09428af59923caae
Song Qiang <qiang.song@usc.edu>
2013-10-01 17:47:09 -0700

    indent regions with whitespaces

--- monitor-hpc-jobs.sh
+++ monitor-hpc-jobs.sh
@@ -2,12 +2,11 @@
 # Song Qiang <keeyang@ustc.edu>
 
 ### validate if a particular job is successfully finished
+# usage:
+# validate_job read_file 0 1000000 # for single end
+# validate_job read_file1 read_file1 0 1000000 # for pair end
 function validate_job # readfile, start, number
 {
-	# usage:
-	# validate_job read_file 0 1000000 # for single end
-	# validate_job read_file1 read_file1 0 1000000 # for pair end
-	
     if [ "$#" == "3" ];
     then
         readfile=$(readlink -f $1);
@@ -18,7 +17,7 @@ function validate_job # readfile, start, number
         mrFile=$d/$id-$start-$number.mr;
         logFile=$d/log/$id-$start-$number.log;
         [ -f "$mrFile" ] \
-            && test $( find $mrFile -mmin +10 ) \
+            && test `find $mrFile -mmin +10` \
             && [ -f "$logFile" ] \
             && grep -q "TOTAL READS MAPPED" $logFile;
     else
@@ -31,81 +30,81 @@ function validate_job # readfile, start, number
         mrFile=$d/$id-$start-$number.mr;
         logFile=$d/log/$id-$start-$number.log;
         [ -f "$mrFile" ] \
-            && test $( find $mrFile -mmin +10 ) \
+            && test `find $mrFile -mmin +10` \
             && [ -f "$logFile" ] \
             && [ $(grep "MAPPED:" $logFile|wc -l) -eq 2 ];
-    fi  
+    fi
 }  
 
 function clean_finished_single_end_jobs # workdir
 {
-	workdir=$1;
-	cd $workdir;
-	lc=~qiangson/app/methpipe/trunk/bin/lc_approx;
-	number=1000000
-	finished=0;
-	todo=0;
-	for f in $(find -L $PWD -name "*.fastq");
-	do
-		total=$(echo $($lc -n 10 -z 1000000 $f|cut -f2)/4|bc)   
-		failedjobs=0
-		for start in $(seq 0 $number $total)  
-		do
-			validate_job $f $start $number;
-			if [ "$?" -ne 0 ];
-			then
-				failedjobs=$(echo $failedjobs + 1|bc);
-				break;
-			fi
-		done
-		
-		if [ $failedjobs -eq 0 ];
-		then
-			rm -f $f;
-			finished=$(echo $finished + 1|bc);
-		else
-			todo=$(echo $todo + 1|bc);
-		fi
-	done
-	
-	date
-	echo Finished=$finished, TODO=$todo; 
+    workdir=$1;
+    cd $workdir;
+    lc=~qiangson/app/methpipe/trunk/bin/lc_approx;
+    number=1000000;
+    finished=0;
+    todo=0;
+    for f in $(find -L $PWD -name "*.fastq");
+    do
+        total=$(echo $($lc -n 10 -z 1000000 $f|cut -f2)/4|bc);
+        failedjobs=0;
+        for start in $(seq 0 $number $total);
+        do
+            validate_job $f $start $number;
+            if [ "$?" -ne 0 ];
+            then
+                failedjobs=$(echo $failedjobs + 1|bc);
+                break;
+            fi
+        done
+        
+        if [ $failedjobs -eq 0 ];
+        then
+            rm -f $f;
+            finished=$(echo $finished + 1|bc);
+        else
+            todo=$(echo $todo + 1|bc);
+        fi
+    done
+    
+    date;
+    echo Finished=$finished, TODO=$todo; 
 }
 
 function clean_finished_paired_end_jobs # workdir
 {
-	workdir=$1;
-	cd $workdir;
-	lc=~qiangson/app/methpipe/trunk/bin/lc_approx;
-	number=1000000
-	finished=0;
-	todo=0;
-	for f in $(find $PWD -name "*_1.fastq");
-	do
-		readTrichFile=$f
-		readArichFile=${f/_1.fastq/_2.fastq}
-		total=$(echo $($lc -n 10 -z 1000000 $f|cut -f2)/4|bc)   
-		failedjobs=0
-		for start in $(seq 0 $number $total)  
-		do
-			validate_job $readTrichFile $readArichFile $start $number;
-			if [ "$?" -ne 0 ];
-			then
-				failedjobs=$(echo $failedjobs + 1|bc);
-				break;
-			fi
-		done
-		
-		if [ $failedjobs -eq 0 ];
-		then
-			rm -f $readTrichFile $readArichFile;
-			finished=$(echo $finished + 1|bc);
-		else
-			todo=$(echo $todo + 1|bc);
-		fi
-	done
-	
-	date
-	echo Finished=$finished, TODO=$todo; 
+    workdir=$1;
+    cd $workdir;
+    lc=~qiangson/app/methpipe/trunk/bin/lc_approx;
+    number=1000000;
+    finished=0;
+    todo=0;
+    for f in $(find $PWD -name "*_1.fastq");
+    do
+        readTrichFile=$f;
+        readArichFile=${f/_1.fastq/_2.fastq};
+        total=$(echo $($lc -n 10 -z 1000000 $f|cut -f2)/4|bc);
+        failedjobs=0;
+        for start in $(seq 0 $number $total);
+        do
+            validate_job $readTrichFile $readArichFile $start $number;
+            if [ "$?" -ne 0 ];
+            then
+                failedjobs=$(echo $failedjobs + 1|bc);
+                break;
+            fi
+        done
+        
+        if [ $failedjobs -eq 0 ];
+        then
+            rm -f $readTrichFile $readArichFile;
+            finished=$(echo $finished + 1|bc);
+        else
+            todo=$(echo $todo + 1|bc);
+        fi
+    done
+    
+    date;
+    echo Finished=$finished, TODO=$todo; 
 }
 
