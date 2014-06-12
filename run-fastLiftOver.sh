a1dbcc4b5690e256045f58ba5d6aa6c71dfce624
Song Qiang <songqiang@users.noreply.github.com>
2014-06-11 17:41:18 -0700

    add copyright info

--- run-fastLiftOver.sh
+++ run-fastLiftOver.sh
@@ -1,3 +1,23 @@
+# run-fastLiftOver.sh: example code to map mouse mehtcount file to human 
+# reference genome with the fastLiftOver tool.
+#
+# Copyright (C) 2014 University of Southern California and
+# Song Qiang <keeyang@ustc.edu>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 fromFile=Mouse_ESC.meth
 indexFile=CpGs-mm9Tohg19.bed
 toFile=Mouse_ESC-hg19.meth
