3252198ef5a3968b855dbf3958b03544f6e80624
Song Qiang <keeyang@ustc.edu>
2014-08-26 22:47:39 -0700

    add callback script for methpipe master

---
+++ BaseSpace/methpipe-master-callback.js
@@ -0,0 +1,44 @@
+function launchSpec(dataProvider)
+{
+    var retval = {
+        commandLine: ["python", "/home/methpipe/methpipe-wrapper/methpipe-master-wrapper.py"],
+        containerImageId:"songqiang/methpipe"
+    };
+	
+    return retval;
+}
+
+
+// example multi-node launch spec
+/*
+function launchSpec(dataProvider)
+{
+    var retval = {
+        nodes: []
+    };
+	
+    retval.nodes.push({
+        appSessionName: "Hello World 1",
+        commandLine: ["/helloWorld.sh", "$AccessToken", "$AppSessionId", "$ApiUrl"],
+        containerImageId:"tliu1/helloworld"
+    });
+	
+    retval.nodes.push({
+        appSessionName: "Hello World 2",
+        commandLine: ["/helloWorld.sh", "$AccessToken", "$AppSessionId", "$ApiUrl"],
+        containerImageId:"tliu1/helloworld"
+    });
+	
+    return retval;
+}
+*/
+
+/* 
+function billingSpec(dataProvider) {
+    return [
+    {
+        "Id" : "insert product ID here",
+        "Quantity": 1.0
+    }];
+}
+*/
